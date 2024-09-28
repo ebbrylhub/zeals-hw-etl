0c824f78508bdeb4c3144f505fa088184e051360
ebbrylhub <ebbryl.connect@gmail.com>
2024-09-29 00:23:07 +0700

    make use of jinja template for yesterday processing

--- dags/bikeshare_etl.py
+++ dags/bikeshare_etl.py
@@ -1,20 +1,23 @@
 from airflow import DAG
 from airflow.operators.python_operator import PythonOperator
 from datetime import datetime, timedelta
+import logging
 from scripts.etl_bq_to_gcs import extract_bigquery_bikeshare
 from scripts.etl_gcs_to_bq_biglake import create_external_table_bikeshare
 
 default_args = {
     'owner': 'airflow',
-    'start_date': datetime(2024, 9, 27),
+    'start_date': datetime(2024, 9, 26),
     'retries': 1,
     'retry_delay': timedelta(minutes=5),
 }
 
 dag = DAG('bikeshare_etl', default_args=default_args, schedule_interval='@daily')
 
-def extract_bikeshare():
-    extract_bigquery_bikeshare()
+def extract_bikeshare(**kwargs):
+    previous_day = kwargs['ds']
+    logging.info(f"Extracting bikeshare data for previous day: {previous_day}")
+    extract_bigquery_bikeshare(previous_day)
     pass
 
 def create_biglake_bikeshare():
@@ -24,6 +27,7 @@ def create_biglake_bikeshare():
 extract_task = PythonOperator(
     task_id='extract_bikeshare',
     python_callable=extract_bikeshare,
+    provide_context=True,
     dag=dag,
 )
 
--- dags/scripts/etl_bq_to_gcs.py
+++ dags/scripts/etl_bq_to_gcs.py
@@ -1,10 +1,11 @@
 from datetime import datetime, timedelta
 from google.cloud import bigquery
 from google.cloud import storage
+import logging
 import os
 import pandas as pd
 
-def extract_bigquery_bikeshare():
+def extract_bigquery_bikeshare(processing_date):
     # Prepare Environment Variables
     bucket_name = os.environ['GCS_BUCKET_NAME']
     
@@ -12,13 +13,10 @@ def extract_bigquery_bikeshare():
     client = bigquery.Client()
     storage_client = storage.Client()
 
-    # Get the previous day's date
-    previous_day = (datetime(2024, 6, 30) - timedelta(1)).strftime('%Y-%m-%d')
-
     # Define the query to extract data for the previous day
     query = f"""
     SELECT * FROM `bigquery-public-data.austin_bikeshare.bikeshare_trips`
-    WHERE DATE(start_time) = '{previous_day}'
+    WHERE DATE(start_time) = '{processing_date}'
     """
 
     # Run the query and load results into a Pandas DataFrame
@@ -34,7 +32,7 @@ def extract_bigquery_bikeshare():
         hourly_data = df[pd.to_datetime(df['start_time']).dt.hour == hour]
 
         # Define the folder path using the start_time date and hour
-        folder_path = f'bikeshare/date={previous_day}/hour={hour:02d}/data.parquet'
+        folder_path = f'bikeshare/date={processing_date}/hour={hour:02d}/data.parquet'
         
         # Save hourly DataFrame to Parquet format locally
         hourly_data.to_parquet("data.parquet")
@@ -44,7 +42,7 @@ def extract_bigquery_bikeshare():
         blob = bucket.blob(folder_path)
         blob.upload_from_filename("data.parquet")
 
-        print(f"Data for {previous_day} at hour {hour:02d} uploaded to {folder_path} in GCS.")
+        print(f"Data for {processing_date} at hour {hour:02d} uploaded to {folder_path} in GCS.")
 
 if __name__ == "__main__":
     pass
